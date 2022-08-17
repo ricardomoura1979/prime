3baa3a62412ab82ac4f64f83bd6f2a6127a516d3
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-17 16:24:21 -0500

    Update index.js

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+
+
+
+
+
 function Filme(){
+  const { id } = useParams();
+  const [filme, setFilme] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    async function loadFilme(){
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "7e96bffe0134b12ce878a2723cbeb600",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(()=>{
+        console.log("Filme não encontrado!")
+      })
+    }
+    loadFilme();
+
+    return() => {
+      console.log("COMPONENTE FOI DESMONTADO")
+    }
+  },[])
+  if(loading){
+    return(
+      <div className="filme-info">
+       
+      </div>
+    )
+  }
+
+
+
   return(
-    <div>
-      <h1>BEM VINDO AOS DETALHES DOS FILMES</h1>
+    <div className= "filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <h3>Sinopse</h3>
+      <span> {filme.overview}</span>
+      <strong> Avaliação: {filme.vote_average} / 10</strong>
     </div>
   )
 }
