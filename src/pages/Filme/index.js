39030d698c23a6a153d7654b6d091c6e1024c122
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-19 09:14:44 -0500

    addes save film in the localstorage

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -36,6 +36,24 @@ function Filme(){
       console.log("COMPONENTE FOI DESMONTADO")
     }
   },[navigate, id])
+
+function salvarFilme(){
+  const minhaLista = localStorage.getItem("@primeflix");
+  
+  let filmesSalvos = JSON.parse(minhaLista) || [];
+
+  const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id) 
+  if(hasFilme){
+    alert("Esse filme já está na lista!")
+    return;
+
+  }
+filmesSalvos.push(filme);
+localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+alert("Filme salvo com sucesso")
+}
+
+
   if(loading){
     return(
       <div className="filme-info">
@@ -54,7 +72,7 @@ function Filme(){
       <span> {filme.overview}</span>
       <strong> Avaliação: {filme.vote_average} / 10</strong>
       <div className="area-buttons"> 
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
           <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>Trailer</a>
         </button>
