7e2332de299282c5b5266000469ef608dcd3640a
ricardomoura1979 <73795249+ricardomoura1979@users.noreply.github.com>
2022-08-20 06:25:27 -0500

    created favourites page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Favoritos from './pages/Favoritos';
 import Header from './components/Header';
-import Erro from './pages/Erro'
+import Erro from './pages/Erro';
+
+
+
+
 function RoutesApp() {
   return(
     <BrowserRouter>
@@ -11,6 +16,7 @@ function RoutesApp() {
       <Route path="/" element={ <Home/> } />
       
       <Route path="/filme/:id" element={ <Filme/> } />
+      <Route path="/favoritos" element={ <Favoritos/> } />
 
       <Route path="*" element={<Erro/>}/>
 
